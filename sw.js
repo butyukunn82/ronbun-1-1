3b96e5395a6790c4dbd5d0b9898abd25e0d2fd56
butyukunn82 <butyukunn82@gmail.com>
2026-09-02 13:01:03 +0900

    Sync ST Reflex Trainer assets and fix direct data loading

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="st-reflex-v2";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./data/questions.js","./data/glossary.js","./questions.js","./glossary.js"];
+const CACHE="st-reflex-v3";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./data/questions.js","./data/glossary.js","./icons/icon-192.png","./icons/icon.svg"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
